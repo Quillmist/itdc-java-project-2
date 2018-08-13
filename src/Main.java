8cd87af2c9445af0518d600c182b7d0658614a8c
marlenabdullaev <marlen.abdullaev@gmail.com>
2018-08-13 19:59:28 +0600

    Создать и настроить окно приложения #1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
+import javax.swing.*;
+
 public class Main {
+    private JPanel mainPanel;
+    private JList roomList;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("План здания");
+        frame.setContentPane(new Main().mainPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1280,720);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
