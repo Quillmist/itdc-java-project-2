4562cb05925969f65d1d738318cadcc2c28d90fd
marlenabdullaev <marlen.abdullaev@gmail.com>
2018-08-13 20:29:01 +0600

    Создать базовый интерфейс для редактирования списка комнат в здании #2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,10 @@ import javax.swing.*;
 public class Main {
     private JPanel mainPanel;
     private JList roomList;
+    private JTextField roomNameText;
+    private JTextArea roomDescriptionText;
+    private JButton addRoomButton;
+    private JButton removeRoomButton;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("План здания");
